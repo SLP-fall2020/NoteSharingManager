170c5170fda3296d89b640d75ab893d7bb9c92fd
lengocnu8497 <lengocnu8497.kl@gmail.com>
2020-09-26 13:20:31 -0700

    delete secrets

--- server/server/settings.py
+++ server/server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y4hqh*x*(p@_vj98p3)%4i#k^8ziv)$*u(a^i@a^@6ei2=7t1*'
+# SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
